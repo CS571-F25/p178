f79d8cb3269a6022ece903dbc09452e24c539a8f
rosegie <shadowrose219@gmail.com>
2025-10-31 18:27:26 -0500

    first commit

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,6 @@
+export default function AboutMe(props) {
+return <div>
+    <h1>About Me</h1>
+    <h2><em>Under Construction</em></h2>
+</div>
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home(props) {
+return <div>
+    <h1>Home</h1>
+    <h2><em>Under Construction</em></h2>
+</div>
+}
